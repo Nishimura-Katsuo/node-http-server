e41fb7fdc85499eb31fa8d28ca1670a14b4567b2
Nishimura-Katsuo <jwebbb01@hotmail.com>
2019-04-07 05:41:35 +0000

    API Scripts now loaded as modules

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 "use strict";
-/* globals global require process logDir htmlDocs logger Buffer */
+/* globals global require process logDir htmlDocs logger Buffer __dirname */
 
 // the ws and compression modules are from npm
 // I think express is too?
@@ -11,18 +11,40 @@ const http = require('http');
 const https = require('https');
 const WebSocket = require('ws');
 const fs = require('fs');
-const vm = require('vm');
 const domain = require('domain');
 const express = require('express');
 const compression = require('compression');
+const acceptedHosts = ["nishicode.com", "www.nishicode.com"];
+//const crypto = require('crypto');
+const badExtension = /\.php(?=\?|$)/;
+const maxPost = 1e4;
+
+let isValidHost = (host) => acceptedHosts.some(check => check === host);
+
+/* function salter (text) {
+	if (!text) {
+		return 0;
+	}
+
+	let ret = 0;
+
+	for (let c = 0; c < text.length; c++) {
+		ret = ((ret << 1) | (ret >> 23)) & 0xFFFFFF;
+		ret ^= text.charCodeAt(c);
+	}
+
+	return ret.toString(16);
+}
+
+let passhash = (pass) => crypto.scryptSync(pass, salter(pass), 64).toString('base64'); */
 
 Object.defineProperties(global, {
 	logDir: {
-		value: './logs',
+		value: __dirname + '/logs',
 		writable: false,
 	},
 	htmlDocs: {
-		value: './html',
+		value: __dirname + '/html',
 		writable: false,
 	},
 });
@@ -49,7 +71,7 @@ Object.defineProperties(global, {
 					fs.mkdirSync(logDir);
 				}
 
-				myout = fs.createWriteStream( logDir + '/' + process.pid + '.txt' );
+				myout = fs.createWriteStream( logDir + '/CONSOLE', {flags: 'a'});
 				myerr = myout;
 			}
 
@@ -59,148 +81,238 @@ Object.defineProperties(global, {
 	},
 });
 
-const scriptcache = {};
+const scripts = new class {
+	constructor () {
+		this.watch = {};
+	}
 
-function loadScript (file) {
-	return new Promise((resolve, reject) => {
-		fs.stat(file, (err, stats) => {
-			if (err) {
-				reject("NOT A FILE");
-			} else if (!(stats.isFile())) {
-				reject("NOT A FILE");
-			} else if (scriptcache[file] && scriptcache[file].mtime === stats.mtime.toUTCString()) {
-				resolve(scriptcache[file]);
-			} else {
-				fs.readFile(file, (errb, data) => {
-					if (errb) {
-						reject(errb);
-					} else {
-						logger.log("Compiling: " + file);
-						scriptcache[file] = {mtime: stats.mtime.toUTCString(), exports: undefined};
-						scriptcache[file].exports = vm.runInThisContext(data, {filename: file, displayErrors: true});
+	load (file) {
+		try {
+			file = require.resolve(file);
 
-						if (typeof scriptcache[file].exports === 'function') {
-							scriptcache[file].exports = new scriptcache[file].exports();
+			if (!this.watch[file]) {
+				logger.log('Loading script: ' + file);
+				this.watch[file] = fs.watch(file, {persistent: false}, () => {
+					try {
+						if (require.cache[file]) {
+							delete require.cache[file];
 						}
 
-						resolve(scriptcache[file]);
+						this.watch[file].close();
+						delete this.watch[file];
+					} catch (err) {
+						logger.log(err.stack);
 					}
 				});
 			}
-		});
+
+			return require(file);
+		} catch (err) {
+			logger.log(err.stack);
+		}
+
+		return null;
+	}
+};
+
+function scriptHandler (req, res, next) {
+	let q = url.parse(req.url, true);
+	let filepathbase = htmlDocs + decodeURI(q.pathname);
+	let postdata = [], postdatalen = 0;
+
+	if (!/\.sss$/i.test(filepathbase)) {
+		filepathbase += ".sss"; // I just use sss for 'server side script'
+	}
+
+	fs.access(filepathbase, fs.constants.R_OK, access_err => {
+		if (access_err) {
+			return next();
+		}
+
+		try {
+			let script = scripts.load(filepathbase), scriptInterface;
+
+			if (script) {
+				if (typeof script.request === 'function') {
+					req.on('data', (chunk) => {
+						postdatalen += chunk.length;
+
+						if (postdatalen > maxPost) {
+							res.status(413).end();
+							res.connection.destroy();
+						} else {
+							postdata.push(chunk);
+						}
+					});
+					req.on('end', () => {
+						new Promise((resolve, reject) => {
+							scriptInterface = {
+								responseCode: 200,
+								headers: {'Content-Type': 'text/plain'},
+								setCookie: {},
+								getPostData: () => Buffer.concat(postdata).toString(),
+								query: q.query,
+								method: req.method,
+								path: q.pathname,
+								resolve: resolve,
+								reject: reject,
+								cookie: (req.headers.cookie ? qs.parse(req.headers.cookie, ';') : {}),
+							};
+							script.request(scriptInterface, logger);
+						}).then(response => {
+							for (let k in scriptInterface.setCookie) {
+								res.setHeader('Set-Cookie', qs.escape(k) + "=" + qs.escape(scriptInterface.setCookie[k]));
+							}
+
+							res.writeHead(scriptInterface.responseCode, scriptInterface.headers);
+
+							if (response !== undefined && response !== null) {
+								res.end(response.toString(), 'binary');
+							} else {
+								res.end();
+							}
+						}).catch(err => {
+							for (let k in scriptInterface.setCookie) {
+								res.setHeader('Set-Cookie', qs.escape(k) + "=" + qs.escape(scriptInterface.setCookie[k]));
+							}
+
+							logger.log(`[${process.pid} @ ${new Date().toUTCString()}] ` + err.stack ? err.stack : err.toString());
+							res.writeHead(500, scriptInterface.headers);
+
+							if (err !== undefined && err !== null) {
+								res.end(err.stack ? err.stack : err.toString(), 'binary');
+							} else {
+								res.end();
+							}
+						});
+					});
+				} else {
+					res.writeHead(500, {'Content-Type': 'text/plain'});
+					res.end('API Loaded Successfully!');
+				}
+			} else {
+				res.writeHead(500, {'Content-Type': 'text/plain'});
+				res.end('Error loading script!');
+			}
+		} catch (err) {
+			logger.log(err);
+		}
+
+		return next();
 	});
 }
 
-function scriptHandler (req, res, next) {
-	if (req.method !== "HEAD" && req.method !== "GET" && req.method !== "POST") {
+function scriptRouter (req, res, next) {
+	let d;
+
+	switch (req.method) {
+	case 'GET':
+	case 'POST':
+	case 'PUT':
+	case 'DELETE':
+		d = domain.create();
+		d.on('error', (err) => {
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+
+			if (err) {
+				logger.log(`[${process.pid} @ ${new Date().toUTCString()}] ` + err.stack ? err.stack : err.toString());
+				res.end(err.stack ? err.stack : err.toString(), 'binary');
+			} else {
+				res.end('Internal Server Error');
+			}
+		});
+		d.run(() => {
+			scriptHandler(req, res, next);
+		});
+
+		break;
+	default:
 		return next();
 	}
+}
 
-	let q = url.parse(req.url, true);
-	let filepathbase = htmlDocs + decodeURI(q.pathname);
-	let postdata = [];
-	const d = domain.create();
-	d.on('error', (err) => {
-		logger.log(err.stack);
-		res.writeHead(500, {'Content-Type': 'text/plain'});
-		res.end(err.stack, 'binary');
+function checkDomain (req, res, next) {
+	let parts = req.headers.host.split('.');
 
-		return;
+	if (parts.length < 3) {
+		logger.log(`[${process.pid} @ ${new Date().toUTCString()}][${req.socket.remoteAddress}] ${req.method} ${JSON.stringify(req.headers.host)}${req.url}\t(forward to www)`);
+		res.writeHead(301, {Location: 'https://www.' + req.headers.host + req.url});
+		res.end();
+
+		return true;
+	}
+
+	return typeof next === 'function' ? next() : false;
+}
+
+function killRequest (req, res, next) {
+	if (req.headers.host && isValidHost(req.headers.host) && !badExtension.test(req.url)) {
+		return typeof next === 'function' ? next() : false;
+	}
+
+	logger.log(`[${process.pid} @ ${new Date().toUTCString()}][${req.socket.remoteAddress}] ${req.method} ${JSON.stringify(req.headers.host) || 'NOHOST'}${req.url}\t(killed)`);
+	let postdatalen = 0, postdata = [];
+	req.on('data', chunk => {
+		postdatalen += chunk.length;
+
+		if (postdatalen > maxPost) {
+			res.destroy();
+		}
+
+		postdata.push(chunk);
 	});
-	d.run(() => {
-		req.on('data', (chunk) => {
-			if (postdata.length > 1e6) {
-				res.status(413).end();
-				res.connection.destroy();
-			} else {
-				postdata.push(chunk);
-			}
-		});
-		req.on('end', () => {
-			if (!/\.sss$/i.test(filepathbase)) {
-				filepathbase += ".sss"; // I just use sss for 'server side script'
-			}
+	req.on('end', () => {
+		logger.log(`\n[${process.pid} @ ${new Date().toUTCString()}][${req.socket.remoteAddress}]\n### Headers ###: ${JSON.stringify(req.rawHeaders)}`);
 
-			let scriptInterface = {headers: {'Content-Type': 'text/plain'}};
+		if (postdata.length) {
+			logger.log('### DATA ###: ' + JSON.stringify(Buffer.concat(postdata).toString()));
+		}
 
-			loadScript(filepathbase).then(cache => new Promise((resolve, reject) => {
-				if (req.method === "HEAD") {
-					scriptInterface.responseCode = 200;
-					scriptInterface.headers['Last-Modified'] = cache.mtime;
-					resolve();
-				} else {
-					if (typeof cache.exports.request === 'function') {
-						Object.assign(scriptInterface, {
-							responseCode: 200,
-							headers: {'Content-Type': 'text/plain'},
-							setCookie: {},
-							getPostData: () => Buffer.concat(postdata).toString(),
-							query: q.query,
-							method: req.method,
-							path: q.pathname,
-							resolve: resolve,
-							reject: reject,
-							cookie: (req.headers.cookie ? qs.parse(req.headers.cookie, ';') : {}),
-						});
-						cache.exports.request(scriptInterface, logger);
-					} else {
-						reject("UNSUPPORTED SCRIPT");
-					}
-				}
-			})).then(response => {
-				for (let k in scriptInterface.setCookie) {
-					res.setHeader('Set-Cookie', qs.escape(k) + "=" + qs.escape(scriptInterface.setCookie[k]));
-				}
+		logger.log('### END ###\n');
 
-				res.writeHead(scriptInterface.responseCode, scriptInterface.headers);
-
-				if (response !== undefined && response !== null) {
-					res.end(response.toString(), 'binary');
-				} else {
-					res.end();
-				}
-			}).catch(err => {
-				if (err === "NOT A FILE") {
-					next();
-				} else {
-					for (let k in scriptInterface.setCookie) {
-						res.setHeader('Set-Cookie', qs.escape(k) + "=" + qs.escape(scriptInterface.setCookie[k]));
-					}
-
-					logger.log(err.stack ? err.stack : err.toString());
-					res.writeHead(500, scriptInterface.headers);
-
-					if (err !== undefined && err !== null) {
-						res.end(err.stack ? err.stack : err.toString(), 'binary');
-					} else {
-						res.end();
-					}
-				}
+		try {
+			res.writeHead(404, {
+				'Content-Type': 'text/html',
 			});
-		});
+			fs.createReadStream('./badreply.html').pipe(res);
+		} catch (err) {
+			logger.log(err);
+		}
 	});
+
+	return true;
+}
+
+function logRequest (req, res, next) {
+	logger.log(`[${process.pid} @ ${new Date().toUTCString()}][${req.ip}] ${req.method} ${JSON.stringify(req.headers.host)}${req.url}\t(OK)`);
+
+	return next();
 }
 
 if (cluster.isMaster) { // master code
 	console.log('Spawning children...');
 	cluster.fork(); // only fork once, this just allows the server to keep itself alive
 	cluster.on('exit', (worker /*, code, signal */) => {
-		console.log(`worker ${worker.process.pid} died`);
+		console.log(`[${process.pid} @ ${new Date().toUTCString()}] Child ${worker.process.pid} died!`);
 		cluster.fork(); // if server dies fork a new thread
 	});
 } else { // child code
-	logger.log('Starting node HTTP Server [' + process.pid + ']');
+	logger.log(`[${process.pid} @ ${new Date().toUTCString()}] Starting node HTTP Server`);
 	process.on('exit', (code) => {
-		logger.log(`Exiting with code: ${code}\n`);
+		logger.log(`[${process.pid} @ ${new Date().toUTCString()}] Exiting with code: ${code}\n`);
 	});
 
-	let msg404 = ["Not found!!!", "Recheck your URL, bruh", "I cahna do et, Captain!"];
+	let msg404 = ["404 Not Found", "404 (That resource does not exist)", "404 (Check your URL)"];
 
 	// server setup
 	const server = express();
-	const staticOpts = { extensions: ['.html', '.htm', '.json', '.lib'] };
+	const staticOpts = { extensions: ['.html', '.htm', '.json'], maxAge: 600000 };
+
+	server.use(killRequest); // kill connection if host name is not specified
+	server.use(checkDomain); // redirects to www if no subdomain exists
+	server.use(logRequest); // logs incoming requests
 	server.use(compression());
-	server.use(scriptHandler);
+	server.use(scriptRouter);
 	server.use(express.static('html', staticOpts));
 	server.use(function (req, res) {
 		let msg = msg404[Math.floor(Math.random() * msg404.length)];
@@ -219,36 +331,41 @@ if (cluster.isMaster) { // master code
 	}, server);
 	let wss = new WebSocket.Server({ server: httpserver });
 	wss.on('connection', (ws, req) => {
-		let q = url.parse(req.url, true);
-		let filepathbase = htmlDocs + decodeURI(q.pathname);
+		try {
+			logger.log(`[${process.pid} @ ${new Date().toUTCString()}][${req.socket.remoteAddress}] ${req.method} ${JSON.stringify(req.headers.host)}${req.url}\t(websocket)`);
+			let q = url.parse(req.url, true);
+			let filepathbase = htmlDocs + decodeURI(q.pathname);
 
-		if (!/\.sss$/i.test(filepathbase)) {
-			filepathbase += ".sss";
-		}
+			if (!/\.sss$/i.test(filepathbase)) {
+				filepathbase += ".sss";
+			}
 
-		loadScript(filepathbase).then(cache => {
-			if (cache && cache.exports && typeof cache.exports.connect === 'function') {
+			let script = scripts.load(filepathbase);
+
+			if (script && typeof script.connect === 'function') {
 				ws.scriptPath = filepathbase;
 
-				if (!cache.exports.connect(ws, wss, req)) {
-					ws.terminate();
+				if (script.connect(ws, wss, req)) {
+					return;
 				}
-			} else {
-				ws.terminate();
 			}
-		}).catch(() => {
-			ws.terminate();
-		});
+		} catch (err) {
+			logger.log(err);
+		}
+
+		ws.terminate();
 	});
 	httpserver.listen(443);
 
 	// forward non-secure requests to https
-	http.createServer(/* server */ (req, res) => {
-		res.writeHead(307, {'Location': 'https://' + req.headers.host + req.url});
-		res.end();
+	http.createServer((req, res) => {
+		if (!killRequest(req, res) && !checkDomain(req, res)) {
+			logger.log(`[${process.pid} @ ${new Date().toUTCString()}][${req.socket.remoteAddress}] ${req.method} ${JSON.stringify(req.headers.host)}${req.url}\t(forward to https)`);
+			res.writeHead(301, {Location: 'https://' + req.headers.host + req.url});
+			res.end();
+		}
 	}).on("connection", (sock) => {
 		sock.setNoDelay(true);
 	}).listen(80);
-
 }
 
